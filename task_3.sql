e08f306b765796352f6bda5346578a81d3a90057
degembaeva <tkandybekovna@gmail.com>
2025-04-22 17:19:18 +0600

    update task_3

--- task_3.sql
+++ task_3.sql
@@ -107,6 +107,107 @@ SET buy = IF(buy > amount, amount, buy),
     price = IF(buy = 0, price*0.9, price)
     
 
+/*Пример
+
+Если в таблице supply  есть те же книги, что и в таблице book, добавлять эти книги в таблицу book не имеет смысла. 
+Необходимо увеличить их количество на значение столбца amountтаблицы supply.*/
+
+
+UPDATE book, supply 
+SET book.amount = book.amount + supply.amount
+WHERE book.title = supply.title AND book.author = supply.author;
+
+SELECT * FROM book;
+
+
+
+
+
+/*Задание
+Для тех книг в таблице book , которые есть в таблице supply, не только увеличить их количество в таблице book 
+( увеличить их количество на значение столбца amountтаблицы supply), но и пересчитать их цену 
+(для каждой книги найти сумму цен из таблиц book и supply и разделить на 2).*/
+
+
+UPDATE book, supply
+SET book.amount = supply.amount + book.amount,
+    book.price = (book.price+supply.price)/2
+WHERE book.title = supply.title AND book.author = supply.author
+
+
+
+
+/*Пример
+
+Удалить из таблицы supply все книги, названия которых есть в таблице book.*/
+
+DELETE FROM supply 
+WHERE title IN (
+        SELECT title 
+        FROM book
+      );
+
+
+SELECT * FROM supply;
+
+/*Задание
+Удалить из таблицы supply книги тех авторов, общее количество экземпляров книг которых в таблице book превышает 10.*/
+DELETE FROM supply
+WHERE author IN (SELECT author FROM book
+                   GROUP BY author
+                   HAVING SUM(amount) > 10)
+                   
+                   
+/*Пример
+Создать таблицу заказ (ordering), куда включить авторов и названия тех книг, количество экземпляров которых 
+в таблице book меньше 4. Для всех книг указать одинаковое количество экземпляров 5.*/
+
+
+CREATE TABLE ordering AS
+SELECT author, title, 5 AS amount
+FROM book
+WHERE amount < 4;
+
+SELECT * FROM ordering;
+
+
+/*Пример
+
+Создать таблицу заказ (ordering), куда включить авторов и названия тех книг, количество экземпляров которых в 
+таблице book меньше 4. Для всех книг указать одинаковое значение - среднее количество экземпляров книг 
+в таблице book.*/
+
+
+CREATE TABLE ordering AS
+SELECT author, title, 
+   (
+    SELECT ROUND(AVG(amount)) 
+    FROM book
+   ) AS amount
+FROM book
+WHERE amount < 4;
+
+SELECT * FROM ordering;
+
+
+/*Задание
+Создать таблицу заказ (ordering), куда включить авторов и названия тех книг, количество экземпляров 
+которых в таблице book меньше среднего количества экземпляров книг в таблице book. 
+В таблицу включить столбец   amount, в котором для всех книг указать одинаковое значение - среднее количество 
+экземпляров книг в таблице book.*/
+
+CREATE TABLE ordering AS
+SELECT author, title,
+       (SELECT ROUND(AVG(amount)) FROM book) AS amount
+FROM book
+WHERE amount < (
+       SELECT AVG(amount) FROM book
+);
+
+
+
+
+
 
 
 
